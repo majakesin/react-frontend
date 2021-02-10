08e9f2549a29024e48d0e700d93e92cb4781fdaf
majakesin <kesin.maja@gmail.com>
2021-02-10 10:39:16 +0100

    #Created code structure

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function rootReducer(state = {}, action) {
+  /* primer vracanja reducera
+    return {
+        // the value of `state.todos` is whatever the todos reducer returns
+        todos: todosReducer(state.todos, action),
+        // For both reducers, we only pass in their slice of the state
+        filters: filtersReducer(state.filters, action)
+        */
+}
+export default rootReducer;
