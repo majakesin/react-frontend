b3163efab9236d5d178c741ac86ac5745c09336c
majakesin <kesin.maja@gmail.com>
2021-02-10 11:02:52 +0100

    #Changed rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,12 +1,5 @@
+import { combineReducers } from "@reduxjs/toolkit";
 import React from "react";
 
-function rootReducer(state = {}, action) {
-  /* primer vracanja reducera
-    return {
-        // the value of `state.todos` is whatever the todos reducer returns
-        todos: todosReducer(state.todos, action),
-        // For both reducers, we only pass in their slice of the state
-        filters: filtersReducer(state.filters, action)
-        */
-}
+const rootReducer = combineReducers({});
 export default rootReducer;
